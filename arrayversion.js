fb05fdecf851007354732b97438801e5f75fe94d
Kelly <ibekellyy@gmail.com>
2018-08-25 15:10:01 -0400

    note to work on later- make sure that error does not show

--- arrayversion.js
+++ arrayversion.js
@@ -7,6 +7,11 @@ var foodProducts = [
      'image': "./plantain.jpg",
      'stars': '4',
      'calories': '140',
+     'reviews':
+      [ {name: "Kelly",
+        text: "This is so delicious!"
+      }
+      ]
     },
 
     {'product': "trader joe's everything but the bagel sesame seasoning blend",
@@ -14,6 +19,11 @@ var foodProducts = [
      'image': "./bagel.jpg",
      'stars': '4.5',
      'calories': '5',
+      'reviews':
+      [ {name: "Kelly",
+        text: "This is so delicious!"
+      }
+      ]
     }
 
 ];
@@ -39,29 +49,31 @@ $(document).ready(function() {
     // if (foodProducts[0]["product"].includes($userInput)) {
     //   //any of one word that you include in the serach will match produc string
     //   console.log("Included")
-
     // }
  //we have to loop this because right now
 
+
   for (var i = 0; i < foodProducts.length; i++) {
-    if (foodProducts[i]["product"].includes($userInput)){
+
+//how to do function if find any of the words in the userInput.
+    if (foodProducts[i]["product"].includes($userInput)) {
       console.log(foodProducts[i]["product"], ". product found")
 
+      $(".homepage").hide();
+      $(".results").show();
+
+      createTemplate();
       const $img = $("<img>").attr("src",foodProducts[i]["image"]);
       $(".results").append($img);
-
-
-      //after informa
     }
+
+    else {
+      const $errorDiv = $('<div>')
+      $errorDiv.text("We could not find your search of " +  $input + " in our database. Please try searching for another product.")
+      $(".results").append($errorDiv);
+    }
+
   }
-
-
-    //   attachPlantainInfo();
-    // }
-
-    // else if ($userInput~='sweet','plantain','chips') {
-    //   attachBagelInfo();
-    // }
     event.target.reset()
   })
 
@@ -69,10 +81,8 @@ $(document).ready(function() {
 
 
 
-function printResult() {
+function createTemplate() {
   console.log("function works!")
-
-
 
 }
 
